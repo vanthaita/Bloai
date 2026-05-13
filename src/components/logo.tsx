9268af1ecb84b718f06b819d0378e59052c0de96
vanthaita <thaitv225@gmail.com>
2026-05-13 21:25:51 +0700

    fix: LCP request discovery, Reduce unused JavaScript

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,12 +6,14 @@ const Logo = () => {
   return (
     <Link href='/'>
       <Image
-        src="https://res.cloudinary.com/dq2z27agv/image/upload/q_auto,f_webp,w_1200/v1778080489/vvgbbauib3zgshie7gln.png"
+        src="https://res.cloudinary.com/dq2z27agv/image/upload/q_auto,f_auto,w_250,h_250,c_fill/v1778080489/vvgbbauib3zgshie7gln.png"
         alt="Bloai Logo"
         width={125}
         height={125}
         className='object-cover w-[125px] h-[125px]'
         priority
+        fetchPriority="high"
+        unoptimized
       />
     </Link>
   )
